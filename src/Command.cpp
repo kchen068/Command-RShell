1298249a4e15c8584d5073e0125e2e1101aaae02
kchen068 <kchen068@ucr.edu>
2017-05-31 19:26:13 -0700

    Add files via upload

--- src/Command.cpp
+++ src/Command.cpp
@@ -9,12 +9,14 @@ using namespace boost;
 Command::Command()
 {
     this->status = false;
+    this->test = false;
 }
 
 Command::Command(string s)
 {
     this->status = false;
     this->token = s;
+    this->test = false;
 }
 
 bool Command::statusCheck(){
@@ -37,4 +39,16 @@ void Command::addtoken(string t){
 }
 void Command::addstatus(bool st){
     this->status = st;
+}
+bool Command::istest(){
+    return this->test;
+}
+void Command::addtest(bool tst){
+    this->test = tst;
+}
+void Command::addprec(int i){
+    this->precsize = i;
+}
+int Command::getprec(){
+    return this->precsize;
 }
